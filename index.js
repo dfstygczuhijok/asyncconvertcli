11481ee678f1279c18a485f447c27a9d9cd1c5b8
dfstygczuhijok <ssjchaquille345@hotmail.com>
2024-05-06 08:38:40 +0700

    start

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const reverseString = (str) => str.split("").reverse().join("");
+function findTheDifference(s, t) {
+  let result = 0;
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
+  }
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
+  }
+  return String.fromCharCode(result);
+}
