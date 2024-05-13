e5e0d6abcb6befb741b6f8c422d2dae1aeb08d08
dfstygczuhijok <ssjchaquille345@hotmail.com>
2024-05-14 05:20:54 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-    arr.slice(i * size, i * size + size),
-  );
+function partitionLabels(s) {
+  const last = new Array(26);
+  for (let i = 0; i < s.length; i++) {
+    last[s.charCodeAt(i) - "a".charCodeAt(0)] = i;
+  }
+  let anchor = 0;
+  let j = 0;
+  const result = [];
+  for (let i = 0; i < s.length; i++) {
+    j = Math.max(j, last[s.charCodeAt(i) - "a".charCodeAt(0)]);
+    if (i === j) {
+      result.push(i - anchor + 1);
+      anchor = i + 1;
+    }
+  }
+  return result;
+}
